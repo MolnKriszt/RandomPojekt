23d3d19ba3c46531d2b6c7b43b8569f9bcfdc21b
MolnKriszt <krisz200601@gmail.com>
2024-01-19 10:42:23 +0100

    teszt

--- Teszt3/NumberRandom.cs
+++ Teszt3/NumberRandom.cs
@@ -10,7 +10,7 @@ namespace Test3
     {
         static public void RandomNumber()
         {
-            Console.WriteLine("RandomNumber asdasd");
+            Console.WriteLine("RandomNumber asdasd asd");
             
         }
 
