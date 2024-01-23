738447d11ebfb25cf6150a854a5c936ca20d7ba3
MolnKriszt <krisz200601@gmail.com>
2024-01-23 12:25:26 +0100

    teszt

---
+++ Teszt3/Krisztián/EtelekRandom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszt3.Krisztián
+{
+    internal class EtelekRandom
+    {
+    }
+}
