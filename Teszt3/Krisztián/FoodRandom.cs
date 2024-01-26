2bd25247eb886b0afd2f11454f47a7dbac7cb3cc
MolnKriszt <krisz200601@gmail.com>
2024-01-26 10:15:10 +0100

    Beolvaskész

--- Teszt3/Krisztián/FoodRandom.cs
+++ Teszt3/Krisztián/FoodRandom.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,33 @@ namespace Test3
     static internal partial class MyRandom
     {
         internal static string lang = "hu";
+
+        public static List<Tuple<int, string>> foodlist { get; } = new List<Tuple<int, string>>();
+        public static void FoodReanding()
+        {
+            if (lang == "en")
+            {
+                string[] lines = File.ReadAllLines("etelek.txt");
+                foreach (var line in lines)
+                {
+                    string[] columns = line.Split(';');
+                    int type = int.Parse(columns[0]);
+                    string food = columns[1];
+                    foodlist.Add(new Tuple<int,string>(type, food));
+                }
+            }
+            else if (lang == "hu")
+            {
+                string[] lines = File.ReadAllLines("etelek.txt");
+                foreach (var line in lines)
+                {
+                    string[] columns = line.Split(';');
+                    int type = int.Parse(columns[0]);
+                    string food = columns[1];
+                    foodlist.Add(new Tuple<int, string>(type, food));
+                }
+            }
+        }
 
         static public void RandomFood()
         {
